c0c94bc22d0dceac408823c8cb75ec2d914ebba8
Freddy Harahap <freddy.122140018@student.itera.ac.id>
2025-05-08 22:45:02 +0700

    Inisialisasi template awal frontend
    dc: not finished yet

---
+++ kerkomkuy-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">KerKomKuy</h1>
+      <button className="btn btn-success">Test Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
